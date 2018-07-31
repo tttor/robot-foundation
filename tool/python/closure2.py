498a7687df2d5f6a7fa9cc2bc5fac9259da58685
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-31 15:42:37 +1000

    fix

--- tool/python/closure2.py
+++ tool/python/closure2.py
@@ -2,17 +2,17 @@
 # https://www.learnpython.org/en/Closures
 
 def main():
-    val = 10
+    val = 10000
 
     def closure(val_mod):
         nonlocal val
+        val = 0
         val += val_mod
         print('closure(): val=', val)
 
         def fn_inside_closure(val_mod_2):
-            resp = val + val_mod_2
-            print('fn_inside_closure(): resp=', resp)
-            return resp
+            print('fn_inside_closure(): val=', val)
+            return val + val_mod_2
 
         return fn_inside_closure
 
@@ -22,9 +22,9 @@ def run_closure(closure_fn, val_mod):
     fn_from_closure = closure_fn(val_mod=val_mod)
     print(fn_from_closure(5))
 
-    fn_from_closure_2 = closure_fn(val_mod=2)
-    print(fn_from_closure(5))
+    fn_from_closure_2 = closure_fn(val_mod=10)
     print(fn_from_closure_2(5))
+    print(fn_from_closure(5))
 
 if __name__ == '__main__':
     main()
